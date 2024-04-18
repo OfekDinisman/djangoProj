162ac6b16c0aba1378fc638846b3748a3efd4661
Ofek Dinisman <45730373+OfekDinisman@users.noreply.github.com>
2024-04-18 18:35:56 +0300

    Add files via upload

---
+++ myapp/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+def todos(request):
+    return render(request,"todos.html")
